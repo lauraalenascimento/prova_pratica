3e7dd7d4d78697f42c78871a1de6ef1e0a1a1acb
Laura Nascimento <110117512+lauraalenascimento@users.noreply.github.com>
2023-07-10 18:20:41 -0300

    Add files via upload

---
+++ prova pratica/index.js
@@ -0,0 +1,36 @@
+// Atividade 01:
+
+// const http = require('http')
+// const port = 2000
+
+// const server = http.createServer((req,res)=>{
+//     res.setHeader('content-type' :'application/json')
+//     res.end()
+// })
+// server.listen('servidor ${port}')
+
+// Atividade 02:
+
+// const path = require('path')
+// const https = '//al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf'
+
+//  path.basename()
+
+
+// Atividade 03:
+
+ // Atividade 04:
+// const fs = require('fs')
+
+// fs.rename('texto.txt','teste.txtx',(err) =>{
+//     if(err) throw err;
+//     console.log('arquivo!')
+// })
+
+ // Atividade 05:
+
+
+
+ 
+
+
